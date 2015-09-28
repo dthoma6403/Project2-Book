9b038d824c83fc24a7b050e13aa60dd38041ae27
dthoma6403 <G00045325@W205-11.academic.raritanval.edu>
2015-09-28 12:02:29 -0400

    Problems 2.83 - 2.86
    First half of the assignment

--- Book.java
+++ Book.java
@@ -3,24 +3,66 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
- */
+ * @author Dean Thomas
+ * @version 9/27/2015
+ */;
 class Book
 {
     // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
-
-    // Add the methods here ...
+// Book Problem # 2.83
+    public String getAuthor ()
+    {
+        return author;
+    }
+    
+        public String getTitle ()
+    {
+        return title;
+    }
+    
+    // Book Problem # 2.84
+    
+   public void printAuthor()
+   {
+       System.out.println ("This masterpiece is graciously brought to you by " + author);
+    }
+    
+     public void printTitle()
+   {
+       System.out.println ("This masterpiece is elegantly titled " + title);
+    }
+    
+    // Book Problem # 2.85
+    
+            public int getPages ()
+    {
+        return pages;
+    }
+    
+    // Book Problem # 2.86
+    
+         public void printDetails()
+   {
+       System.out.println ("Title: " + title);
+       System.out.println ("Author: " + author);
+       System.out.println ("Pages: " + pages);
+    }
+    
+    // Book Problem # 2.87
+    
+    
 }
